f3596d9be047d5913c4ffad1c9a74c78e374d7f1
Opemipo Ogunkola <ogunks@live.com>
2020-12-07 15:19:55 +0000

    clean day07 - part 2

--- 2020/day07/part2.py
+++ 2020/day07/part2.py
@@ -2,38 +2,23 @@ import re
 from collections import defaultdict
 
 
-LINE = re.compile(r'^([a-z]+ [a-z]+) bags contain ([0-9])+ ([a-z]+ [a-z]+) bags?')
-BAG_REGEX = re.compile(r'^, ([0-9]+) ([a-z]+ [a-z]+) bags?')
-NO_OTHER_BAG_RE = re.compile(r'^([a-z]* [a-z]*) bags contain no other bags.$')
+LINE_REGEX = re.compile(r'^(?P<container_bag>[a-z]+ [a-z]+) bags contain (?P<bag_count>[0-9]+) (?P<bag>[a-z]+ [a-z]+) bags?')
+BAG_REGEX = re.compile(r', (?P<bag_count>[0-9]+) (?P<bag>[a-z]+ [a-z]+) bags?')
 
 
 def parse_graph():
     lines = [line.strip() for line in open('input.txt')]
     graph = defaultdict(list)
     for line in lines:
-        match = NO_OTHER_BAG_RE.match(line)
+        match = LINE_REGEX.match(line)
         if match:
-            continue
-        match = LINE.match(line)
-        container = match.group(1)
-        n = int(match.group(2))
-        bag = match.group(3)
-        graph[container].extend([bag]*n)
-        if match:
-            _, end = match.span()
-            line = line[end:]
-            while line and line != '.':
-                match = BAG_REGEX.match(line)
-                n = int(match.group(1))
-                bag = match.group(2)
-                # print(bag)
-                graph[container].extend([bag]*n)
-
-                _, end = match.span()
-                line = line[end:]
-    # print(graph)
+            container = match.group('container_bag')
+            bag = match.group('bag')
+            graph[container].extend([bag]*int(match.group('bag_count')))
+            for match in BAG_REGEX.finditer(string=line, pos=match.span()[1]):
+                bag = match.group('bag')
+                graph[container].extend([bag]*int(match.group('bag_count')))
     return graph
-
 
 def parse_bag(line):
     bag, rest_of_line = parse_words(line=line, n=2)
@@ -45,7 +30,7 @@ def number_of_unique_ancestors(graph):
     while stack:
         bag = stack.pop()
         count += 1
-        for container_bag in graph.get(bag, []):
+        for container_bag in graph[bag]:
             stack.append(container_bag)
     return count - 1
 
