98e8cd44c7a73525c4d683dc3ab601cec4618f54
Opemipo Ogunkola <ogunks@live.com>
2020-12-13 23:56:06 +0000

    Use complex number to represent the position

--- 2020/day12/part1.py
+++ 2020/day12/part1.py
@@ -1,30 +1,30 @@
 from dataclasses import dataclass
 import math
-
+import cmath
 
 def next_state(state, instruction):
-    current_direction, (current_x, current_y) = state
+    current_direction, position = state
     direction, value = instruction
     if direction == 'N':
-        return (current_direction, (current_x, current_y + value))
+        return (current_direction, position + value*1j)
     elif direction == 'S':
-        return (current_direction, (current_x, current_y - value))
+        return (current_direction, position - value*1j)
     elif direction == 'W':
-        return (current_direction, (current_x - value, current_y))
+        return (current_direction, position - value)
     elif direction == 'E':
-        return (current_direction, (current_x + value, current_y))
+        return (current_direction, position + value)
     elif direction == 'L':
-        return (current_direction+value, (current_x, current_y))
+        return (current_direction+value, position)
     elif direction == 'R':
-        return (current_direction-value, (current_x, current_y))
+        return (current_direction-value, position)
     elif direction == 'F':
-        return (current_direction, (current_x+(value*math.cos(math.pi/180*current_direction)), current_y+(value*math.sin(math.pi/180*current_direction))))
+        return (current_direction, position + value*cmath.rect(1, current_direction*math.pi/180))
 
 
 if __name__ == "__main__":
     instructions = [(line[0], int(line[1:])) for line in (line.strip() for line in open('input.txt'))]
-    state = (0, (0, 0))
+    state = (0, 0)
     for instruction in instructions:
         state = next_state(state, instruction)
-    direction, (x, y) = state
-    print(abs(x) + abs(y))
+    direction, position = state
+    print(abs(position.real) + abs(position.imag))
