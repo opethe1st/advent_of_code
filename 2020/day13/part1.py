f10a0152227c3a899b72cde5b1df45726d2f0495
Opemipo Ogunkola <ogunks@live.com>
2020-12-20 09:04:27 +0000

    format day13 solutions

--- 2020/day13/part1.py
+++ 2020/day13/part1.py
@@ -1,15 +1,13 @@
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     earliest_time_to_depart = int(input())
-    bus_ids = list(map(int, (num for num in input().split(',') if num != 'x')))
+    bus_ids = list(map(int, (num for num in input().split(",") if num != "x")))
     # print(list(bus_ids))
     departure_time = earliest_time_to_depart
     found = False
     while departure_time < earliest_time_to_depart + min(bus_ids) + 1:
         for bus_id in bus_ids:
-            if (departure_time % bus_id == 0):
-                print(bus_id*(departure_time-earliest_time_to_depart))
+            if departure_time % bus_id == 0:
+                print(bus_id * (departure_time - earliest_time_to_depart))
                 found = True
                 break
         if found:
--- 2020/day13/part2.py
+++ 2020/day13/part2.py
@@ -14,12 +14,21 @@ def solve(x, y):
     # print('x, y, gcd, m, n, -', x, y, g, m, n, m*x+n*y)
     return -n
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     earliest_time_to_depart = int(input())
-    values = [value for value in input().split(',')]
-    bus_ids = [int(value) for value in values if value != 'x']
-    positions = {int(value): i for i, value in enumerate(values) if value != 'x'}
+    values = [value for value in input().split(",")]
+    bus_ids = [int(value) for value in values if value != "x"]
+    positions = {int(value): i for i, value in enumerate(values) if value != "x"}
 
     lowest_common_multiple = lcm(*bus_ids)
-    # print(lowest_common_multiple)
-    print(sum(positions[b]*solve(b, lowest_common_multiple//b)*lowest_common_multiple//b for b in bus_ids)%lowest_common_multiple)
+    print(
+        sum(
+            positions[b]
+            * solve(b, lowest_common_multiple // b)
+            * lowest_common_multiple
+            // b
+            for b in bus_ids
+        )
+        % lowest_common_multiple
+    )
