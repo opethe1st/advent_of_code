a94ed728708e6aff3173904c025a7d4e42afa68c
Opemipo Ogunkola <ogunks@live.com>
2020-12-20 09:02:57 +0000

    format day12 solution

--- 2020/day12/part1.py
+++ 2020/day12/part1.py
@@ -1,28 +1,34 @@
-from dataclasses import dataclass
 import math
 import cmath
+
 
 def next_state(state, instruction):
     current_direction, position = state
     direction, value = instruction
-    if direction == 'N':
-        return (current_direction, position + value*1j)
-    elif direction == 'S':
-        return (current_direction, position - value*1j)
-    elif direction == 'W':
+    if direction == "N":
+        return (current_direction, position + value * 1j)
+    elif direction == "S":
+        return (current_direction, position - value * 1j)
+    elif direction == "W":
         return (current_direction, position - value)
-    elif direction == 'E':
+    elif direction == "E":
         return (current_direction, position + value)
-    elif direction == 'L':
-        return (current_direction+value, position)
-    elif direction == 'R':
-        return (current_direction-value, position)
-    elif direction == 'F':
-        return (current_direction, position + value*cmath.rect(1, current_direction*math.pi/180))
+    elif direction == "L":
+        return (current_direction + value, position)
+    elif direction == "R":
+        return (current_direction - value, position)
+    elif direction == "F":
+        return (
+            current_direction,
+            position + value * cmath.rect(1, current_direction * math.pi / 180),
+        )
 
 
 if __name__ == "__main__":
-    instructions = [(line[0], int(line[1:])) for line in (line.strip() for line in open('input.txt'))]
+    instructions = [
+        (line[0], int(line[1:]))
+        for line in (line.strip() for line in open("input.txt"))
+    ]
     state = (0, 0)
     for instruction in instructions:
         state = next_state(state, instruction)
--- 2020/day12/part2.py
+++ 2020/day12/part2.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 import math
 import cmath
 
@@ -6,24 +5,30 @@ import cmath
 def next_state(state, instruction):
     waypoint_position, ship_position = state
     direction, value = instruction
-    if direction == 'N':
-        return (waypoint_position + value*1j , ship_position)
-    elif direction == 'S':
-        return (waypoint_position - value*1j, ship_position)
-    elif direction == 'W':
+    if direction == "N":
+        return (waypoint_position + value * 1j, ship_position)
+    elif direction == "S":
+        return (waypoint_position - value * 1j, ship_position)
+    elif direction == "W":
         return (waypoint_position - value, ship_position)
-    elif direction == 'E':
+    elif direction == "E":
         return (waypoint_position + value, ship_position)
-    elif direction == 'L':
-        return (waypoint_position*cmath.rect(1, math.pi/180*value), ship_position)
-    elif direction == 'R':
-        return (waypoint_position*cmath.rect(1, -math.pi/180*value), ship_position)
-    elif direction == 'F':
-        return (waypoint_position, ship_position+value*waypoint_position)
+    elif direction == "L":
+        return (waypoint_position * cmath.rect(1, math.pi / 180 * value), ship_position)
+    elif direction == "R":
+        return (
+            waypoint_position * cmath.rect(1, -math.pi / 180 * value),
+            ship_position,
+        )
+    elif direction == "F":
+        return (waypoint_position, ship_position + value * waypoint_position)
 
 
 if __name__ == "__main__":
-    instructions = [(line[0], int(line[1:])) for line in (line.strip() for line in open('input.txt'))]
+    instructions = [
+        (line[0], int(line[1:]))
+        for line in (line.strip() for line in open("input.txt"))
+    ]
     state = ((10 + 1j), 0)
     for instruction in instructions:
         state = next_state(state, instruction)
