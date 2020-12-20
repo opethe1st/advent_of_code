229f4969521bdb4b0b4e7e760a1f6aa008124cab
Opemipo Ogunkola <ogunks@live.com>
2020-12-20 09:13:59 +0000

    format day 14 part2

--- 2020/day14/part2.py
+++ 2020/day14/part2.py
@@ -1,16 +1,16 @@
 import re
 
 
-MASK_RE = re.compile(r'^mask = (.*)$')
-MEM_RE = re.compile(r'^mem\[(\d+)\] = (\d+)$')
+MASK_RE = re.compile(r"^mask = (.*)$")
+MEM_RE = re.compile(r"^mem\[(\d+)\] = (\d+)$")
 
 
 def ones(mask):
     value = 0
     power = 0
     for bit in mask[::-1]:
-        if bit == '1':
-            value += (1 << power)
+        if bit == "1":
+            value += 1 << power
         power += 1
     return value
 
@@ -19,27 +19,30 @@ def zeros(mask):
     value = 0
     power = 0
     for bit in mask[::-1]:
-        if bit != '0':
-            value += (1 << power)
+        if bit != "0":
+            value += 1 << power
         power += 1
     return value
 
 
 def generate_address(address, mask):
     address_in_bit = bin(address)[2:]
-    address_in_bit = '0' * (36 - len(address_in_bit)) + address_in_bit
+    address_in_bit = "0" * (36 - len(address_in_bit)) + address_in_bit
     # print('a, m', address_in_bit, mask)
-    address_in_bit = ['1' if mask_bit == '1' else 'X' if mask_bit == 'X' else address_bit for address_bit, mask_bit in zip(address_in_bit, mask)]
+    address_in_bit = [
+        "1" if mask_bit == "1" else "X" if mask_bit == "X" else address_bit
+        for address_bit, mask_bit in zip(address_in_bit, mask)
+    ]
     # print('after mask', "".join(address_in_bit))
-    count = (1<<address_in_bit.count('X'))
+    count = 1 << address_in_bit.count("X")
     for i in range(count):
-        value = ''
+        value = ""
         for letter in address_in_bit:
-            if letter == '1':
-                value += '1'
-            elif letter == '0':
+            if letter == "1":
+                value += "1"
+            elif letter == "0":
                 value += letter
-            elif letter == 'X':
+            elif letter == "X":
                 value += str(i & 1)
                 i >>= 1
         yield value
@@ -67,7 +70,7 @@ def solve(instructions):
     return sum(memory.values())
 
 
-if __name__ == '__main__':
-    instructions = [line.strip() for line in open('test.txt')]
+if __name__ == "__main__":
+    instructions = [line.strip() for line in open("test.txt")]
     ans = solve(instructions)
     print(ans)
