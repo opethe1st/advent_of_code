d728a60d7c5d15c66fc726f531f3d3abe9666c62
Opemipo Ogunkola <7265129+opethe1st@users.noreply.github.com>
2021-11-01 13:56:36 +0000

    solution to 2015/day10

---
+++ 2015/day10/part2.py
@@ -0,0 +1,16 @@
+from itertools import groupby
+
+
+def look_and_say(seq):
+    s = ''
+    for k, g in groupby(seq):
+        s += str(len(list(g)))
+        s += k
+    return s
+
+
+if __name__ == "__main__":
+    seq = "1321131112"
+    for _ in range(50):
+        seq = look_and_say(seq)
+    print(len(seq))
