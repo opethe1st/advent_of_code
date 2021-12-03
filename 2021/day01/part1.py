1136b54187a16d6990bf46a72ba0f9e3c7a8f3b9
Opemipo Ogunkola <7265129+opethe1st@users.noreply.github.com>
2021-12-03 17:35:12 +0000

    solutions to 2021/day01

---
+++ 2021/day01/part1.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    depths = [int(depth) for depth in open('input.txt')]
+    print(
+        sum(
+            next_depth > depth
+            for depth, next_depth in zip(depths, depths[1:])
+        )
+
+    )
---
+++ 2021/day01/part2.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    depths = [int(depth) for depth in open('input.txt')]
+    print(
+        sum(
+            next_depth > depth
+            for depth, next_depth in zip(depths, depths[3:])
+        )
+    )
