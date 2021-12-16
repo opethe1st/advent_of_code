e445f3ef73abc262c9f94b28bd346d4bb8fdcac0
Opemipo Ogunkola <7265129+opethe1st@users.noreply.github.com>
2021-12-16 23:12:12 +0000

    solution to 2021/day16

---
+++ 2021/day16/part1.py
@@ -0,0 +1,118 @@
+def read_version(stream: str, cursor: int):
+    return int(stream[cursor : cursor + 3], 2), cursor + 3
+
+
+def read_type_id(stream: str, cursor: int):
+    return int(stream[cursor : cursor + 3], 2), cursor + 3
+
+
+def read_number(stream: str, cursor: int):
+    num = ""
+    while stream[cursor] == "1":
+        num += stream[cursor + 1 : cursor + 5]
+        cursor += 5
+    num += stream[cursor + 1 : cursor + 5]
+    cursor += 5
+    return num, cursor
+
+
+def read_length_type_id(stream: str, cursor: int):
+    return stream[cursor], cursor + 1
+
+
+def read_total_length_in_bits(stream: str, cursor: int):
+    return int(stream[cursor : cursor + 15], 2), cursor + 15
+
+
+def read_total_number_of_subpackets(stream: str, cursor: int):
+    return int(stream[cursor : cursor + 11], 2), cursor + 11
+
+
+def read_subpackets_with_total_length_in_bits(
+    stream: str, cursor: int, total_length_in_bits: int
+):
+    subpackets = []
+    start = cursor
+    while cursor < (start + total_length_in_bits) < len(stream):
+        subpacket, cursor = read_packet(stream, cursor)
+        subpackets.append(subpacket)
+        total_length_in_bits -= 1
+    return subpackets, cursor
+
+
+def read_subpacket_with_number_of_subpackets(
+    stream: str, cursor: int, num_of_subpackets: int
+):
+    subpackets = []
+    while num_of_subpackets and cursor < len(stream):
+        subpacket, cursor = read_packet(stream, cursor)
+        subpackets.append(subpacket)
+        num_of_subpackets -= 1
+    return subpackets, cursor
+
+
+def read_packet(stream: str, cursor: int):
+    version, cursor = read_version(stream, cursor=cursor)
+    type_id, cursor = read_type_id(stream, cursor)
+
+    if type_id == 4:
+        number, cursor = read_number(stream, cursor)
+        return (version, type_id, number), cursor
+    else:
+        length_type_id, cursor = read_length_type_id(stream, cursor)
+
+        if length_type_id == "0":
+            total_length, cursor = read_total_length_in_bits(stream, cursor)
+            subpackets, cursor = read_subpackets_with_total_length_in_bits(
+                stream, cursor, total_length_in_bits=total_length
+            )
+        else:
+            total_number_of_packets, cursor = read_total_number_of_subpackets(
+                stream, cursor
+            )
+            subpackets, cursor = read_subpacket_with_number_of_subpackets(
+                stream, cursor, total_number_of_packets
+            )
+        return (version, type_id, subpackets), cursor
+
+
+def sum_version_numbers(packet):
+    match packet:
+        case ((version, _, str())):
+            return version
+        case ((version, _, list() as packets)):
+            return version + sum(sum_version_numbers(p) for p in packets)
+        case _:
+            raise Exception(packet)
+
+
+hex_to_binary = {
+    "0": "0000",
+    "1": "0001",
+    "2": "0010",
+    "3": "0011",
+    "4": "0100",
+    "5": "0101",
+    "6": "0110",
+    "7": "0111",
+    "8": "1000",
+    "9": "1001",
+    "A": "1010",
+    "B": "1011",
+    "C": "1100",
+    "D": "1101",
+    "E": "1110",
+    "F": "1111",
+}
+
+
+def convert_to_binary(hex_digits):
+    return "".join(hex_to_binary[digit] for digit in hex_digits)
+
+
+if __name__ == "__main__":
+    with open("input.txt") as file:
+        message = file.read().strip()
+    binary = convert_to_binary(message)
+    packet, cursor = read_packet(binary, 0)
+    print(sum_version_numbers(packet))
